bf9b1f234bb955dd82edbb37027d7fbcdac4aa4b
vl4d1m1r0vch4r0v <vladtheluminousravager@gmail.com>
2026-03-13 01:38:08 +0700

    Function D implemented

---
+++ FunctionD.cpp
@@ -0,0 +1,10 @@
+#include "Functions.hpp"
+
+bool IsPalindrome(const std::string& str)
+{
+	for (size_t i = 0, j = str.size() - 1; i < j; ++i, --j)
+		if (str[i] != str[j])
+			return false;
+
+	return true;
+}
