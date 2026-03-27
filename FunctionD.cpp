4e2a040e5a054ea7d76264e505a84162df050f75
vl4d1m1r0vch4r0v <vladtheluminousravager@gmail.com>
2026-03-27 13:00:42 +0700

    circle

--- FunctionD.cpp
+++ FunctionD.cpp
@@ -1,5 +1,22 @@
 #include "Functions.hpp"
+#include <iostream>
+#include <cmath>
 
-void circle()
-{
+struct CircleResult {
+    double circumference;
+    double area;
+    double sectorArea;
+};
+
+// Функция для расчетов. 
+// radius - радиус, angleDegree - угол сектора в градусах (по умолчанию 360)
+CircleResult calculateCircle(double radius, double angleDegree = 360.0) {
+    const double PI = M_PI; // Используем константу из cmath
+    
+    CircleResult res;
+    res.circumference = 2 * PI * radius;
+    res.area = PI * std::pow(radius, 2);
+    res.sectorArea = (res.area * angleDegree) / 360.0;
+    
+    return res;
 }
